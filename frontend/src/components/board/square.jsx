00e8c84fad1d9bd16cfabce0d319f469b8d09b84
Angel <ang_vzrz@live.com.mx>
2022-11-07 01:08:53 -0800

    created utility joinClassNames function for returning single element declaration

--- frontend/src/components/board/square.jsx
+++ frontend/src/components/board/square.jsx
@@ -1,9 +1,18 @@
+const joinClassNames = (...classes) => {
+  return classes.filter(Boolean).join(' ');
+};
+
 function Square({ squareColor }) {
-  if (squareColor === 'dark') {
-    return <td className="w-12 h-12 bg-opacity-90 blur-0 bg-clip-padding bg-gray-800 backdrop-filter backdrop-blur-sm"></td>;
-  } else {
-    return <td className="w-12 h-12 bg-opacity-50 blur-0 bg-clip-padding bg-gray-400 backdrop-filter backdrop-blur-sm"></td>;
-  }
+  return (
+    <td
+      className={joinClassNames(
+        'h-12 w-12 bg-clip-padding blur-0 backdrop-blur-sm backdrop-filter',
+        squareColor === 'dark'
+          ? 'bg-gray-800 bg-opacity-90'
+          : 'bg-gray-400 bg-opacity-50 backdrop-filter'
+      )}
+    ></td>
+  );
 }
 
 export { Square };
