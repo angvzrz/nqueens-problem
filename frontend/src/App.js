9f1c21dea1a559eaa6875be946ff34859815185a
Angel <ang_vzrz@live.com.mx>
2022-11-08 01:30:40 -0800

    styled title and adjusted styling in other components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="w-full overflow-scroll h-screen bg-gradient-to-r from-blue-900 via-sky-700 to-purple-900 p-4 flex flex-col flex-none items-center justify-center">
+    <div className="w-full overflow-scroll h-screen bg-gradient-to-r from-blue-900 via-sky-700 to-purple-900 p-12 flex flex-col flex-none items-center">
       <Title />
       <Home />
     </div>
