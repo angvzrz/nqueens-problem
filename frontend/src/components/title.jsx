768066368f094317058f87530be1896ed32075f7
Angel <ang_vzrz@live.com.mx>
2022-11-07 16:10:16 -0800

    added resources folder and made it absolute path. Initial title component styling

--- frontend/src/components/title.jsx
+++ frontend/src/components/title.jsx
@@ -1,5 +1,12 @@
+import { Queen } from "./board/queen";
+
 function Title() {
-  return <h1>N Queens Problem</h1>;
+  return (
+    <div className="flex items-baseline items-center ">
+      <div className="w-12"><Queen /></div>
+      <h1>N Queens Problem</h1>
+    </div>
+  )
 }
 
 export { Title };
